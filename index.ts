1a2a6752a1653b913c9f07a4d1523c0c656d3c2f
Iulian Ilea <iulian@f9software.com>
2018-04-05 02:34:43 +0300

    Initial version of the Parser.

---
+++ index.ts
@@ -0,0 +1,301 @@
+import * as _ from 'lodash';
+
+export interface ColumnAlias {
+    [key: string]: Select | string;
+}
+
+export type Column = string | ColumnAlias;
+
+export interface TableAlias {
+    [key: string]: string;
+}
+
+export type Table = string | TableAlias;
+
+export interface Sql {
+    sql: string;
+    values?: (string | number)[]
+}
+
+// export interface WhereValueDetailed {
+//     operator: string;
+//     value: string | Select;
+// }
+
+export type WhereValue = string | number | boolean | Select | /*WhereValueDetailed |*/ Sql;
+
+export interface WhereShort {
+    [key: string]: WhereValue
+}
+
+export type Where = string | WhereShort;
+
+export type Limit = number[];
+
+export interface Order {
+    column: string;
+    direction: 'ASC' | 'DESC'
+}
+
+export interface Select {
+    select: Column[];
+    from: Table;
+    where?: Where[];
+    limit?: Limit;
+    order?: Order[]
+}
+
+
+export type Value = string | number | boolean | Select | Sql;
+
+export interface ValueSet {
+    [key: string]: Value;
+}
+
+export interface Insert {
+    insert: ValueSet;
+    into: Table;
+}
+
+export interface Update {
+    update: ValueSet;
+    into: Table;
+    where: Where[]
+}
+
+export interface Delete {
+    deleteFrom: Table;
+    where: Where[];
+}
+
+
+export interface Result {
+    sql: string;
+    values: (string | number | boolean)[]
+}
+
+export type JSql = Select | Insert | Update | Delete;
+
+
+export class Parser {
+    private buffer: string[];
+
+    private values: (string | number | boolean)[];
+
+    parse(jsql: JSql): Result {
+        this.buffer = [];
+        this.values = [];
+
+        if (jsql.hasOwnProperty('select')) {
+            this.parseSelect(<Select> jsql);
+        }
+        else if (jsql.hasOwnProperty('insert')) {
+            this.parseInsert(<Insert> jsql);
+        }
+        else if (jsql.hasOwnProperty('update')) {
+            this.parseUpdate(<Update> jsql);
+        }
+        else if (jsql.hasOwnProperty('deleteFrom')) {
+            // this.parseDelete(<Delete> jsql);
+        }
+
+        const out: Result = {
+            sql: this.buffer.join(''),
+            values: this.values
+        };
+
+        this.buffer = this.values = null;
+
+        return out;
+    }
+
+
+
+    /** Type checkers **/
+
+    private isSelect(jsql: any): boolean {
+        return _.isPlainObject(jsql) && jsql.hasOwnProperty('select');
+    }
+
+    private isSql(jsql: any): boolean {
+        return _.isPlainObject(jsql) && jsql.hasOwnProperty('sql');
+    }
+
+
+
+    /** Parsers **/
+
+    private parseSelect(jsql: Select) {
+        this.buffer.push(' SELECT ');
+
+        jsql.select.map(
+            (column, index: number) => {
+                if (index > 0) {
+                    this.buffer.push(', ');
+                }
+                this.parseColumn(column);
+            }
+        );
+
+        this.buffer.push(' FROM ');
+        this.parseTable(jsql.from);
+
+        if (jsql.where) {
+            this.buffer.push(' WHERE ');
+            jsql.where.forEach(where => this.parseWhere(where));
+        }
+
+        if (jsql.order) {
+            this.buffer.push(' ORDER BY ');
+            jsql.order.forEach((order: Order, index: number) => {
+                if (index > 0) {
+                    this.buffer.push(', ');
+                }
+                this.parseOrder(order);
+            })
+        }
+
+        if (jsql.limit) {
+            this.buffer.push(' LIMIT ');
+            this.parseLimit(jsql.limit);
+        }
+    }
+
+    private parseInsert(jsql: Insert) {
+        const buf = this.buffer;
+        const columns = Object.keys(jsql.insert);
+
+        buf.push(' INSERT INTO ');
+        this.parseTable(jsql.into);
+        buf.push(' (`', columns.join('`, `'), '`) VALUES (');
+
+        columns.forEach(
+            (column, index: number) => {
+                if (index > 0) {
+                    buf.push(', ');
+                }
+                this.parseValue(jsql.insert[column]);
+            }
+        );
+        buf.push(')');
+    }
+
+    private parseUpdate(jsql: Update) {
+        const buf = this.buffer;
+        const columns = Object.keys(jsql.update);
+
+        buf.push(' UPDATE ');
+        this.parseTable(jsql.into);
+        buf.push(' SET ');
+
+        columns.forEach(
+            (column, index: number) => {
+                if (index > 0) {
+                    buf.push(', ');
+                }
+                buf.push('`', column, '` = ');
+                this.parseValue(jsql.update[column]);
+            }
+        );
+
+        buf.push(' WHERE ');
+        jsql.where.forEach(where => this.parseWhere(where));
+    }
+
+
+
+    /** Parser helpers **/
+
+    private parseWhere(where: Where) {
+        if (typeof where === 'string') {
+            this.buffer.push(where);
+        }
+        // parse WhereSHort
+        else if (_.isPlainObject(where)) {
+            Object.keys(where)
+                .forEach((column, index: number) => {
+                    if (index > 0) {
+                        this.buffer.push(' AND ');
+                    }
+
+                    if (typeof column === 'string') {
+                        this.buffer.push('`', column, '` = ');
+                        this.parseValue(where[column]);
+                    }
+                });
+        }
+    }
+
+    private parseValue(value: string | number | boolean | Select | /*WhereValueDetailed |*/ Sql) {
+        const type = typeof value;
+        const buf = this.buffer;
+
+        if (type === 'boolean') {
+            buf.push(value ? '1' : '0');
+        }
+        else if (type === 'string' || type === 'number') {
+            buf.push('?');
+            this.values.push(<string | number> value);
+        }
+        else if (this.isSelect(value)) {
+            buf.push('(');
+            this.parseSelect(<Select> value);
+            buf.push(')');
+        }
+        else if (this.isSql(<string> value)) {
+            buf.push('(');
+            this.parseSql(<Sql> value);
+            buf.push(')');
+        }
+    }
+
+    private parseSql(sql: Sql) {
+        this.buffer.push(sql.sql);
+        Array.prototype.push.apply(this.values, sql.values);
+    }
+
+    private parseOrder(order: Order) {
+        this.buffer.push(order.column, ' ', order.direction);
+    }
+
+    private parseLimit(limit: Limit) {
+        this.buffer.push(limit.join(', '));
+    }
+
+    private parseColumn(column: Column) {
+        if (typeof column === 'string') {
+            this.buffer.push(column);
+        }
+        else if (_.isPlainObject(column)) {
+            Object.keys(column)
+                .map(
+                    alias => {
+                        this.buffer.push('(');
+                        if (this.isSelect(column[alias])) {
+                            this.parseSelect(<Select> column[alias]);
+                        }
+                        else {
+                            this.buffer.push(<string> column[alias]);
+                        }
+                        this.buffer.push(') AS ', alias);
+                    }
+                )
+        }
+    }
+
+    private parseTable(table: Table) {
+        if (typeof table === 'string') {
+            this.buffer.push(table);
+        }
+        else if (_.isPlainObject(table)) {
+            Object.keys(table)
+                .forEach(
+                    alias => {
+                        this.buffer.push('(');
+                        this.parseTable(table[alias]);
+                        this.buffer.push(') AS ', alias);
+                    }
+                )
+        }
+    }
+}
